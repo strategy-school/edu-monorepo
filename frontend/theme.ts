9fde7e2b14ae944a624472e4337357f8435b5a69
Emir Marat <emmarrat@gmail.com>
2023-04-07 19:42:14 +0600

    feat: set up the theme
     added three colors from the brandbook, the font is open sans. work is underway on the integration of gotham pro fonts

--- frontend/theme.ts
+++ frontend/theme.ts
@@ -1,6 +1,42 @@
-import { createTheme } from '@mui/material';
+import { createTheme, Theme } from '@mui/material/styles';
 
-const theme = createTheme({
+interface MyPaletteOptions {
+  custom: {
+    main: string;
+  };
+}
+
+declare module '@mui/material/styles' {
+  interface PaletteOptions {
+    custom?: {
+      main?: string;
+    };
+  }
+}
+
+const myPalette: MyPaletteOptions = {
+  custom: {
+    main: 'rgb(180, 40, 45)',
+  },
+};
+
+const theme: Theme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgb(88, 89, 91)',
+    },
+    secondary: {
+      main: 'rgb(217, 39, 45)',
+    },
+    custom: myPalette.custom,
+  },
+  typography: {
+    fontFamily: 'Open Sans, sans-serif',
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
   components: {
     MuiTextField: {
       defaultProps: {
