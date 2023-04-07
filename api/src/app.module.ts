cd9d3a55160a90d27cc0a4b7e6b663cf74824d78
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-04-07 16:25:50 +0600

    feat: Installed mongoose passport bcrypt for api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/strategy-school'),
+    MongooseModule.forFeature([]),
+  ],
   controllers: [],
   providers: [],
 })
