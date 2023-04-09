96643df537480afebfe134f0871756895f2d227f
Emir Marat <emmarrat@gmail.com>
2023-04-09 23:20:26 +0600

    feat: added 3 schemas: user, type, course
    added auth directory with token-auth.guard, permit.guard, auth.service and local.strategy
    
    also added .editorconfig

---
+++ api/src/auth/token-auth.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { User, UserDocument } from '../schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Request } from 'express';
+
+@Injectable()
+export class TokenAuthGuard implements CanActivate {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest() as Request;
+    const token = request.get('Authorization');
+    if (!token) {
+      return false;
+    }
+
+    const user = await this.userModel.findOne({ token });
+
+    if (!user) {
+      return false;
+    }
+
+    request.user = user;
+    return true;
+  }
+}
---
+++ api/src/schemas/user.schema.ts
@@ -0,0 +1,78 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as crypto from 'crypto';
+import * as bcrypt from 'bcrypt';
+
+const SALT_WORK_FACTOR = 10;
+
+export type UserDocument = User & Document;
+
+enum Role {
+  User = 'user',
+  Teacher = 'teacher',
+  Admin = 'admin',
+}
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  token: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  phoneNumber: string;
+
+  @Prop({ required: true, enum: Role, default: Role.User })
+  role: string;
+
+  @Prop()
+  googleId: string;
+
+  @Prop()
+  isBanned: boolean;
+
+  @Prop([{ type: String, ref: 'Course' }])
+  courses: string[];
+
+  checkPassword: (password: string) => Promise<boolean>;
+  generateToken: () => void;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods.generateToken = function () {
+  this.token = crypto.randomUUID();
+};
+
+UserSchema.methods.checkPassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    delete ret.password;
+    return ret;
+  },
+});
