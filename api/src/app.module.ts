c314dd6ede266804e162da88cf8723bd960ad11a
Emir Marat <emmarrat@gmail.com>
2023-04-09 23:24:15 +0600

    fix: spacing in imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import {Course, CourseSchema} from './schemas/course.schema';
-import {Type, TypeSchema} from './schemas/type.schema';
-import {User, UserSchema} from './schemas/user.schema';
-import {AuthService} from './auth/auth.service';
-import {LocalStrategy} from './auth/local.strategy';
+import { Course, CourseSchema } from './schemas/course.schema';
+import { Type, TypeSchema } from './schemas/type.schema';
+import { User, UserSchema } from './schemas/user.schema';
+import { AuthService } from './auth/auth.service';
+import { LocalStrategy } from './auth/local.strategy';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/strategy-school'),
     MongooseModule.forFeature([{ name: Course.name, schema: CourseSchema }]),
     MongooseModule.forFeature([{ name: Type.name, schema: TypeSchema }]),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),  ],
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [],
   providers: [AuthService, LocalStrategy],
 })
