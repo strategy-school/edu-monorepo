d409f369b64b57f62626aaea3e247f35f8350322
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-04-20 00:12:58 +0600

    feat: Added guards to transactions' routers

--- backend/src/routers/transactions.ts
+++ backend/src/routers/transactions.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { Error } from 'mongoose';
+import auth, { RequestWithUser } from '../middleware/auth';
+import permit from '../middleware/permit';
 import Transaction from '../models/Transactions';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (req, res, next) => {
+transactionsRouter.get('/', auth, permit('admin'), async (req, res, next) => {
   try {
     const userId = req.query.user as string;
     const courseId = req.query.course as string;
@@ -41,29 +43,35 @@ transactionsRouter.get('/', async (req, res, next) => {
   }
 });
 
-transactionsRouter.get('/:id', async (req, res, next) => {
-  try {
-    const transactionId = req.params.id;
-    const transaction = await Transaction.findById(transactionId)
-      .populate('course', 'title price type start_date end_date duration')
-      .populate('user', 'email firstName lastName phoneNumber');
+transactionsRouter.get(
+  '/:id',
+  auth,
+  permit('admin'),
+  async (req, res, next) => {
+    try {
+      const transactionId = req.params.id;
+      const transaction = await Transaction.findById(transactionId)
+        .populate('course', 'title price type start_date end_date duration')
+        .populate('user', 'email firstName lastName phoneNumber');
 
-    if (!transaction) {
-      return res.status(404).send({ error: 'Transaction is not found' });
+      if (!transaction) {
+        return res.status(404).send({ error: 'Transaction is not found' });
+      }
+
+      return res.send({ message: 'Transaction is found', result: transaction });
+    } catch (e) {
+      return next(e);
     }
+  },
+);
 
-    return res.send({ message: 'Transaction is found', result: transaction });
-  } catch (e) {
-    return next(e);
-  }
-});
-
-transactionsRouter.post('/', async (req, res, next) => {
+transactionsRouter.post('/', auth, async (req, res, next) => {
   try {
+    const { user } = req as RequestWithUser;
     const courseId = req.body.course;
 
     const transaction = await Transaction.create({
-      user: req.body.user,
+      user: user._id,
       course: courseId,
     });
 
@@ -83,88 +91,106 @@ transactionsRouter.post('/', async (req, res, next) => {
   }
 });
 
-transactionsRouter.put('/:id', async (req, res, next) => {
-  try {
-    const transactionId = req.params.id;
-    const { user, course, isPaid } = req.body;
+transactionsRouter.put(
+  '/:id',
+  auth,
+  permit('admin'),
+  async (req, res, next) => {
+    try {
+      const transactionId = req.params.id;
+      const { user, course, isPaid } = req.body;
 
-    if (!transactionId) {
-      return res.status(404).send({ error: 'Transaction ID is required' });
+      if (!transactionId) {
+        return res.status(404).send({ error: 'Transaction ID is required' });
+      }
+
+      const transaction = await Transaction.findByIdAndUpdate(
+        transactionId,
+        { user, course, isPaid },
+        { new: true, runValidators: true },
+      )
+        .populate('course', 'title price type start_date end_date duration')
+        .populate('user', 'email firstName lastName phoneNumber');
+
+      if (!transaction) {
+        return res.status(404).send('Transaction not found');
+      }
+
+      return res.send({
+        message: 'Transaction is updated',
+        result: transaction,
+      });
+    } catch (e) {
+      if (e instanceof Error.ValidationError) {
+        return res.status(400).send(e);
+      } else {
+        return next(e);
+      }
     }
+  },
+);
 
-    const transaction = await Transaction.findByIdAndUpdate(
-      transactionId,
-      { user, course, isPaid },
-      { new: true, runValidators: true },
-    )
-      .populate('course', 'title price type start_date end_date duration')
-      .populate('user', 'email firstName lastName phoneNumber');
+transactionsRouter.patch(
+  '/:id/markAsPaid',
+  auth,
+  permit('admin'),
+  async (req, res, next) => {
+    try {
+      const transactionId = req.params.id;
 
-    if (!transaction) {
-      return res.status(404).send('Transaction not found');
+      if (!transactionId) {
+        return res.status(404).send({ error: 'Transaction ID is required' });
+      }
+
+      const transaction = await Transaction.findById(transactionId)
+        .populate('course', 'title price type start_date end_date duration')
+        .populate('user', 'email firstName lastName phoneNumber');
+
+      if (!transaction) {
+        return res.status(404).send({ error: 'Transaction not found' });
+      }
+
+      transaction.isPaid = 'paid';
+
+      await transaction.save();
+
+      return res.send({
+        message: 'Transaction is marked as paid',
+        result: transaction,
+      });
+    } catch (e) {
+      if (e instanceof Error.ValidationError) {
+        return res.status(400).send(e);
+      } else {
+        return next(e);
+      }
     }
+  },
+);
 
-    return res.send({ message: 'Transaction is updated', result: transaction });
-  } catch (e) {
-    if (e instanceof Error.ValidationError) {
-      return res.status(400).send(e);
-    } else {
+transactionsRouter.delete(
+  '/:id',
+  auth,
+  permit('admin'),
+  async (req, res, next) => {
+    try {
+      const transactionId = req.params.id;
+
+      if (!transactionId) {
+        return res.status(400).send({ error: 'Transaction ID is required' });
+      }
+
+      const transaction = await Transaction.findByIdAndDelete(transactionId);
+
+      if (!transaction) {
+        return res.status(404).send({ error: 'Transaction is not found' });
+      }
+
+      return res.send({ message: 'Task is deleted' });
+    } catch (e) {
       return next(e);
     }
-  }
-});
-
-transactionsRouter.patch('/:id/markAsPaid', async (req, res, next) => {
-  try {
-    const transactionId = req.params.id;
-
-    if (!transactionId) {
-      return res.status(404).send({ error: 'Transaction ID is required' });
-    }
-
-    const transaction = await Transaction.findById(transactionId)
-      .populate('course', 'title price type start_date end_date duration')
-      .populate('user', 'email firstName lastName phoneNumber');
-
-    if (!transaction) {
-      return res.status(404).send({ error: 'Transaction not found' });
-    }
-
-    transaction.isPaid = 'paid';
-
-    await transaction.save();
-
-    return res.send({
-      message: 'Transaction is marked as paid',
-      result: transaction,
-    });
-  } catch (e) {
-    if (e instanceof Error.ValidationError) {
-      return res.status(400).send(e);
-    } else {
-      return next(e);
-    }
-  }
-});
-
-transactionsRouter.delete('/:id', async (req, res, next) => {
-  try {
-    const transactionId = req.params.id;
-
-    if (!transactionId) {
-      return res.status(400).send({ error: 'Transaction ID is required' });
-    }
-
-    const transaction = await Transaction.findByIdAndDelete(transactionId);
-
-    if (!transaction) {
-      return res.status(404).send({ error: 'Transaction is not found' });
-    }
-
-    return res.send({ message: 'Task is deleted' });
-  } catch (e) {
-    return next(e);
-  }
-});
+  },
+);
 
 export default transactionsRouter;
