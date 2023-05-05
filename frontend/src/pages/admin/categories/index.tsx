34fc8cc18cb82c994d7a9e72c81165f2482bd0fc
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-05-05 22:49:52 +0600

    fix: Formatted incoming files with prettier

--- frontend/src/pages/admin/categories/index.tsx
+++ frontend/src/pages/admin/categories/index.tsx
@@ -1,38 +1,33 @@
 import AdminLayout from '@/src/components/UI/AdminLayout/AdminLayout';
-import {
-  Button,
-  Grid,
-  Typography,
-} from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 import Link from 'next/link';
 import React from 'react';
-import CategoryAdmin from "@/src/features/categories/components/CategoryAdmin/CategoryAdmin";
+import CategoryAdmin from '@/src/features/categories/components/CategoryAdmin/CategoryAdmin';
 
 const Categories = () => {
-
   return (
-      <AdminLayout>
-        <Grid container spacing={2} direction="column">
-          <Grid item container xs justifyContent="space-between">
-            <Grid item>
-              <Typography variant="h4">Категории</Typography>
-            </Grid>
-            <Grid item>
-              <Button
-                  component={Link}
-                  href="/admin/categories/new-category"
-                  color="primary"
-                  variant="contained"
-              >
-                Добавить новую категорию
-              </Button>
-            </Grid>
+    <AdminLayout>
+      <Grid container spacing={2} direction="column">
+        <Grid item container xs justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h4">Категории</Typography>
           </Grid>
           <Grid item>
-              <CategoryAdmin/>
+            <Button
+              component={Link}
+              href="/admin/categories/new-category"
+              color="primary"
+              variant="contained"
+            >
+              Добавить новую категорию
+            </Button>
           </Grid>
         </Grid>
-      </AdminLayout>
+        <Grid item>
+          <CategoryAdmin />
+        </Grid>
+      </Grid>
+    </AdminLayout>
   );
 };
 
