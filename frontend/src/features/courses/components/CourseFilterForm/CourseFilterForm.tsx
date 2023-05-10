5cda12c2163af11b340760ff57b2b54471c00cdd
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-05-10 20:02:55 +0600

    feat: Updated course filter to search onChange

--- frontend/src/features/courses/components/CourseFilterForm/CourseFilterForm.tsx
+++ frontend/src/features/courses/components/CourseFilterForm/CourseFilterForm.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '@/src/app/hooks';
+import { selectCategories } from '@/src/dispatchers/categories/categoriesSlice';
+import { fetchCategories } from '@/src/dispatchers/categories/categoriesThunks';
+import { fetchCourses } from '@/src/dispatchers/courses/coursesThunks';
+import { CoursePrice, SearchCourse } from '@/src/types';
 import {
   Button,
-  CircularProgress,
   FormControl,
   FormControlLabel,
   FormLabel,
@@ -13,57 +15,52 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
-import LoadingButton from '@mui/lab/LoadingButton';
-import { selectCoursesFetching } from '@/src/dispatchers/courses/coursesSlice';
-import { fetchCategories } from '@/src/dispatchers/categories/categoriesThunks';
-import {
-  selectCategories,
-  selectCategoriesFetching,
-} from '@/src/dispatchers/categories/categoriesSlice';
-import { fetchCourses } from '@/src/dispatchers/courses/coursesThunks';
+import React from 'react';
+
+const filterState = <T extends Partial<Record<keyof T, T[keyof T]>>>(
+  state: T,
+) =>
+  Object.entries(state).reduce<Partial<T>>((acc, [key, value]) => {
+    if (Boolean(value)) {
+      acc[key as keyof T] = value as T[keyof T];
+    }
+    return acc;
+  }, {});
 
 const CourseFilterForm = () => {
   const dispatch = useAppDispatch();
-  const [state, setState] = useState({
-    level: '',
-    category: '',
-    minPrice: '',
-    maxPrice: '',
-  });
+  const [state, setState] = React.useState<SearchCourse>({});
   const categories = useAppSelector(selectCategories);
-  const categoriesLoading = useAppSelector(selectCategoriesFetching);
-  const loading = useAppSelector(selectCoursesFetching);
 
-  useEffect(() => {
+  React.useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
 
-  const inputChangeHandler = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
+  React.useEffect(() => {
+    dispatch(fetchCourses(state));
+  }, [state, dispatch]);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setState((prevState) => {
-      return { ...prevState, [name]: value };
-    });
+    setState((prev) => ({ ...prev, [name]: value }));
+    setState((prev) => filterState<SearchCourse>(prev));
   };
 
-  const submitFormHandler = async (e: React.FormEvent) => {
-    e.preventDefault();
-    await dispatch(fetchCourses(state));
+  const onPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState((prev) => ({ ...prev, price: { ...prev.price, [name]: value } }));
+    setState((prev) => ({
+      ...prev,
+      price: prev.price && filterState<CoursePrice>(prev.price),
+    }));
   };
 
-  const clearState = async () => {
-    setState({
-      level: '',
-      category: '',
-      minPrice: '',
-      maxPrice: '',
-    });
-    await dispatch(fetchCourses());
+  const clearState = () => {
+    setState({});
   };
 
   return (
-    <form onSubmit={submitFormHandler}>
+    <form>
       <Grid container direction="column" spacing={2}>
         <Grid item xs>
           <Typography variant="h5">Фильтрация курсов</Typography>
@@ -73,8 +70,8 @@ const CourseFilterForm = () => {
           <FormControl>
             <FormLabel>Уровень сложности</FormLabel>
             <RadioGroup
-              value={state.level}
-              onChange={inputChangeHandler}
+              value={state.level ? state.level : ''}
+              onChange={onChange}
               defaultValue=""
               name="level"
             >
@@ -108,14 +105,11 @@ const CourseFilterForm = () => {
             label="Категория"
             select
             name="category"
-            value={state.category}
-            onChange={inputChangeHandler}
+            value={state.category ? state.category : ''}
+            onChange={onChange}
           >
             <MenuItem value="" disabled>
-              Пожалуйста, выберите категорию{' '}
-              {categoriesLoading && (
-                <CircularProgress size={20} sx={{ ml: 1 }} />
-              )}
+              Пожалуйста, выберите категорию
             </MenuItem>
             {categories.map((category) => (
               <MenuItem value={category._id} key={category._id}>
@@ -132,44 +126,27 @@ const CourseFilterForm = () => {
           <Grid container spacing={2}>
             <Grid item xs>
               <TextField
-                id="minPrice"
                 label="От"
-                value={state.minPrice}
-                onChange={inputChangeHandler}
-                name="minPrice"
+                value={state.price && state.price.$gte ? state.price.$gte : ''}
+                onChange={onPriceChange}
+                name="$gte"
                 type="number"
-                InputProps={{ inputProps: { min: 0, step: 1000 } }}
               />
             </Grid>
 
             <Grid item xs>
               <TextField
-                id="maxPrice"
                 label="До"
-                value={state.maxPrice || state.minPrice}
-                onChange={inputChangeHandler}
-                name="maxPrice"
+                value={state.price && state.price.$lte ? state.price.$lte : ''}
+                onChange={onPriceChange}
+                name="$lte"
                 type="number"
-                InputProps={{
-                  inputProps: { min: parseFloat(state.minPrice), step: 1000 },
-                }}
               />
             </Grid>
           </Grid>
         </Grid>
 
         <Grid container item spacing={1} justifyContent="start">
-          <Grid item xs={6} md={12} lg={6}>
-            <LoadingButton
-              loadingIndicator="Loading…"
-              loading={loading}
-              type="submit"
-              color="primary"
-              variant="contained"
-            >
-              Применить
-            </LoadingButton>
-          </Grid>
           <Grid item xs={6} md={12} lg={6}>
             <Button
               type="button"
