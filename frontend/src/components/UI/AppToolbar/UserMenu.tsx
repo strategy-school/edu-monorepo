1b2cdd7a3f4d9921ab7f55af889c95187d52cec9
Dmitry Sagaidak <dm.sagaidak@gmail.com>
2023-05-27 11:52:00 +0600

    Api: created router for counting unchecked notifications, created thunk and slice for this router. Overwritten thunk for fetching all notifications. Overwritten logic for counting unchecked notifications in UserMenu.tsx, moved this logic into AppToolbar.tsx

--- frontend/src/components/UI/AppToolbar/UserMenu.tsx
+++ frontend/src/components/UI/AppToolbar/UserMenu.tsx
@@ -1,39 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { User } from '@/src/types';
 import { Avatar, Badge, Button } from '@mui/material';
 import { apiURL } from '@/src/constants';
 import Link from 'next/link';
-import { useAppDispatch, useAppSelector } from '@/src/store/hooks';
-import { selectNotifications } from '@/src/dispatchers/notifications/notificationsSlice';
-import { fetchNotifications } from '@/src/dispatchers/notifications/notificationsThunks';
 
 interface Props {
   user: User;
+  uncheckedCount: number;
 }
 
-const UserMenu: React.FC<Props> = ({ user }) => {
-  const dispatch = useAppDispatch();
-  const notifications = useAppSelector(selectNotifications);
-  const [uncheckedCount, setUncheckedCount] = useState(0);
-
-  useEffect(() => {
-    if (user.role === 'admin') {
-      void dispatch(fetchNotifications());
-    }
-  }, [dispatch, user.role]);
-
-  useEffect(() => {
-    if (user.role === 'admin') {
-      const count = notifications.reduce((count, notification) => {
-        if (!notification.isChecked) {
-          return count + 1;
-        }
-        return count;
-      }, 0);
-      setUncheckedCount(count);
-    }
-  }, [notifications, user.role]);
-
+const UserMenu: React.FC<Props> = ({ user, uncheckedCount }) => {
   let cardImage = '';
 
   if (user.avatar) {
