8f20353dc7bc71640c92e9c4e5e2f060bae18ba6
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-06-16 18:49:02 +0600

    feat: Changed Google Client ID

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,6 @@
 export const apiURL = 'http://localhost:8000';
 export const GOOGLE_CLIENT_ID =
-  '755891940919-fue5nbo1np8irtc10dma8eekcl6fkctl.apps.googleusercontent.com';
+  '22734562658-0aa218i7sat9v9nbann739c1abi2n5je.apps.googleusercontent.com';
 export const dateFormat = 'DD.MM.YYYY HH:mm';
 export const dateCommentFormat = 'DD.MM.YYYY';
 export const strategiaToken = 'strategiaToken';
